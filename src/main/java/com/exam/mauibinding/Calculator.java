3e959a58cbe7aef330c5c8d673343e7874520d2b
LIT <littoma@hotmail.com>
2024-09-03 21:44:12 +0800

    Add files.

---
+++ src/main/java/com/exam/mauibinding/Calculator.java
@@ -0,0 +1,16 @@
+package com.exam.mauibinding;
+
+public class Calculator {
+    // 默认构造函数
+    public Calculator() {}
+
+    // 加法方法：返回两个整数的和
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // 减法方法：返回两个整数的差
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
